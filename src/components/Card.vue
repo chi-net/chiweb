12a682bcffd4f7bdb75825daea2dae9b5d2c150c
so1ve <nn_201312@163.com>
2021-09-21 20:12:32 +0800

    好耶！更新了一下css

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,33 +1,19 @@
 <template>
-<div id="card-app">
-<slot></slot>
-</div>
+  <div class="card">
+    <slot></slot>
+  </div>
 </template>
 <script>
-export default{
+export default {
 }
 </script>
 
 <style scoped>
-@media screen and (min-width:768px){
-    #card-app{
-        width:30%;
-        margin:16px;
-    }
-    
-}
-@media screen and (max-width:768px){
-    #card-app{
-        width:100%;
-        margin-bottom:8px;
-    }
-    
-}
-#card-app{
-    border-radius:5px;
-    border:3px solid #eee;
-    background-color:rgba(255,255,255,0.7);
-    box-shadow:1px 2px 3px;
-    float:left;
+.card {
+  border-radius: 5px;
+  border: 3px solid #eee;
+  background-color: rgba(255, 255, 255, 0.7);
+  box-shadow: 1px 2px 3px;
+  margin: 10px;
 }
 </style>
