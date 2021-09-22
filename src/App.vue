f3122f494c2f6ea92e2791d0c00ad0595f7f1d5f
so1ve <nn_201312@163.com>
2021-09-22 22:08:58 +0800

    更新CopyRight样式

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,35 @@
 <template>
   <div id="app">
-    <Card>
-      <img src="./assets/icon.jpg" id="chiimg">
-      <h2>chihuo2104の小站</h2>
-      <ToolCard>
-        <div class="btn-group">
-          <button class="mdui-btn-block">&lt;上一张</button>
-          <button class="mdui-btn-block">背景图片</button>
-          <button class="mdui-btn-block">下一张&gt;</button>
+    <div class="container">
+      <Card>
+        <img src="./assets/icon.jpg" id="chiimg">
+        <h2>chihuo2104の小站</h2>
+        <ToolCard>
+          <div class="btn-group">
+            <button class="mdui-btn-block">&lt;上一张</button>
+            <button class="mdui-btn-block">背景图片</button>
+            <button class="mdui-btn-block">下一张&gt;</button>
+          </div>
+        </ToolCard>
+        <div id="show">
+          <button class="mdui-btn-block" onclick='changeCopyright()'>显示版权页</button>
         </div>
-      </ToolCard>
-      <div id="show">
-        <button class="mdui-btn-block" onclick='changeCopyright()'>显示版权页</button>
-      </div>
-    </Card>
-    <Card>
-      <h1>个人网站</h1>
-      <h1>提示！</h1>
-      <h1>本站目前还正在开发中，为您展示的是正在开发的版本，本站暂定版本号：Moe Edition。（版本更新记录详见
-        <a href="//history.moekonnyaku.com/newer-log.html" target="_blank">history.moekonnyaku.com/newer-log.html</a>）建议您去我的
-        <a href="//blog.chihuo2104.dev" target="_blank">blog</a>逛一下~</h1>
-      <h2>占位符啦啦啦啦啦<br><br><br><br><br></h2>
-    </Card>
-    <Card>
-      <h1>友情链接</h1>
-      <h2>占位符啦啦啦啦啦<br><br><br><br><br></h2>
-    </Card>
+      </Card>
+      <Card>
+        <h1>个人网站</h1>
+        <h1>提示！</h1>
+        <h1>本站目前还正在开发中，为您展示的是正在开发的版本，本站暂定版本号：Moe Edition。（版本更新记录详见
+          <a href="//history.moekonnyaku.com/newer-log.html" target="_blank">history.moekonnyaku.com/newer-log.html</a>）建议您去我的
+          <a href="//blog.chihuo2104.dev" target="_blank">blog</a>逛一下~</h1>
+        <h2>占位符啦啦啦啦啦<br><br><br><br><br></h2>
+      </Card>
+      <Card>
+        <h1>友情链接</h1>
+        <h2>占位符啦啦啦啦啦<br><br><br><br><br></h2>
+      </Card>
+    </div>
     <div id="copyright" :class="{'hidden':this.$store.state.isCopyrightHidden}">
-      Powered by chihuo2104 &amp; ray.All rights reserved &copy;2018-{{new Date().getFullYear()}}.<br>Chiweb Application Version:V3.0(Moe)[Preview Edition].Packaged Date:2021-09-20.<br>
+      Powered by chihuo2104 &amp; Ray. All rights reserved &copy;2018-{{new Date().getFullYear()}}.<br>Chiweb Application Version:V3.0(Moe)[Preview Edition].Packaged Date:2021-09-20.<br>
       Engined by <a href="//cn.vuejs.org" target="blank">Vue</a>&amp;<a href="//nodejs.org" target="blank">Node.js</a>&amp;<a href="//webpack.js.org" target="blank">Webpack</a>.
       本站已经在互联网的角落苟且偷生了<span id="checkday">0</span>天<span id="checkhour">0</span>小时<span id="checkmin">0</span>分钟<span id="checksec">0</span>秒
     </div>
@@ -59,14 +61,15 @@ export default {
   font-family: "Noto Serif SC";
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+
+.container {
   text-align: center;
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(calc(100% / 3), 1fr));
+  grid-auto-rows: max-content;
   justify-content: center;
-}
-
-.hidden {
-  bottom: -1000px !important;
+  min-height: 100vh;
 }
 
 #chiimg {
@@ -104,7 +107,7 @@ html {
 }
 
 #copyright {
-  position: absolute;
+  position: relative;
   width: 100%;
   bottom: 0;
   background-color: rgba(255, 255, 255, 0.7);
