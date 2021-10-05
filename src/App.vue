f59665217a603d645a9b19d627b68645b9c847d5
chihuo2104 <chihuoqwefengmi13@qq.com>
2021-10-05 22:39:56 +0800

    Future:Add copyright statement for hypergryph and mihoyo.

--- src/App.vue
+++ src/App.vue
@@ -35,7 +35,8 @@
       <a href="//github.com/chihuo2104/chiweb/" traget="_blank">Chiweb</a> Application Version:V3.0(Moe)[Preview Edition].Packaged Date:2021-10-05.
       <br>
       Engined by <a href="//cn.vuejs.org" target="blank">Vue</a>&amp;<a href="//nodejs.org" target="blank">Node.js</a>&amp;<a href="//webpack.js.org" target="blank">Webpack</a>&amp;<a target="_blank" href="//jenkins.io">Jenkins</a>.
-      本站已经在互联网的角落苟且偷生了<span id="checkday">0</span>天<span id="checkhour">0</span>小时<span id="checkmin">0</span>分钟<span id="checksec">0</span>秒<br>
+      <br>本站已经在互联网的角落苟且偷生了<span id="checkday">0</span>天<span id="checkhour">0</span>小时<span id="checkmin">0</span>分钟<span id="checksec">0</span>秒<br>
+      版权声明：部分背景图片版权由<a href="//hypergryph.com" target="_blank">上海市鹰角网络科技有限公司</a>和<a href="//mihoyo.com" target="_blank">上海市米哈游网络科技有限公司</a>所有
       <a href="//icp.gov.moe/?keyword=20212104" target="_blank">萌ICP备20212104号</a>
     </div>
   </div>
