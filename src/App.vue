178ed70e511ae70923b11feab99db338a0c3a3ab
chihuo2104 <chihuoqwefengmi13@qq.com>
2021-10-06 20:45:16 +0800

    Fixed:mihoyo statement.

--- src/App.vue
+++ src/App.vue
@@ -43,7 +43,7 @@
       <br>
       Engined by <a href="//cn.vuejs.org" target="blank">Vue</a>&amp;<a href="//nodejs.org" target="blank">Node.js</a>&amp;<a href="//webpack.js.org" target="blank">Webpack</a>&amp;<a target="_blank" href="//jenkins.io">Jenkins</a>.<a href="//icp.gov.moe/?keyword=20212104" target="_blank">萌ICP备20212104号</a>
       <br>本站已经在互联网的角落苟且偷生了<span id="checkday">0</span>天<span id="checkhour">0</span>小时<span id="checkmin">0</span>分钟<span id="checksec">0</span>秒。<br>
-      版权声明：部分背景图片版权由<a href="//hypergryph.com" target="_blank">上海市鹰角网络科技有限公司</a>和<a href="//mihoyo.com" target="_blank">上海市米哈游(网络/天命/影铁)科技有限公司</a>所有
+      版权声明：部分背景图片版权由<a href="//hypergryph.com" target="_blank">上海市鹰角网络科技有限公司</a>和<a href="//www.mihoyo.com" target="_blank">上海市米哈游影铁科技有限公司</a>所有
     </div>
   </div>
 </template>
