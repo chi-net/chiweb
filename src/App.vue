cc47f1f0d4fcd5e1545aed39954a6b91a1ef8858
chihuo2104 <chihuoqwefengmi13@qq.com>
2021-10-23 19:57:00 +0800

    Future:Change js pipes into vue.

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
       <a href="//github.com/chihuo2104/chiweb/" target="_blank">Chiweb</a> Application Version:V3.0(Moe)[Preview Edition].Packaged Date:2021-10-17.
       <br>
       Engined by <a href="//cn.vuejs.org" target="blank">Vue</a>&amp;<a href="//nodejs.org" target="blank">Node.js</a>&amp;<a href="//webpack.js.org" target="blank">Webpack</a>&amp;<a target="_blank" href="//jenkins.io">Jenkins</a>.<a href="//icp.gov.moe/?keyword=20212104" target="_blank">萌ICP备20212104号</a>
-      <br>本站已经在互联网的角落苟且偷生了<span id="checkday">0</span>天<span id="checkhour">0</span>小时<span id="checkmin">0</span>分钟<span id="checksec">0</span>秒。<br>
+      <br>本站已经在互联网的角落苟且偷生了<!--{{aliveshow}}--><span id="checkday">0</span>天<span id="checkhour">0</span>小时<span id="checkmin">0</span>分钟<span id="checksec">0</span>秒<br>
       版权声明：部分背景图片版权由<a href="//hypergryph.com" target="_blank">上海市鹰角网络科技有限公司</a>和<a href="//www.mihoyo.com" target="_blank">上海市米哈游影铁科技有限公司</a>所有
     </div>
   </div>
@@ -26,6 +26,28 @@ export default {
   name: 'App',
   components: {
     Toolbar, IndexCard, Friendlink, Gallery
+  },
+  data () {
+    return {
+      alive: {
+        day: 0,
+        hour: 0,
+        min: 0,
+        sec: 0
+      }
+    }
+  },
+  mounted () {
+    this.timer = setInterval(function () {
+      this.alive = { day: 1, hour: 2, min: 3, sec: 4 }
+    }, 1000)
+  },
+  computed: {
+    aliveshow: {
+      get () {
+        return this.alive.day + '日' + this.alive.hour + '时' + this.alive.min + '分' + this.alive.sec + '秒'
+      }
+    }
   }
 }
 </script>
