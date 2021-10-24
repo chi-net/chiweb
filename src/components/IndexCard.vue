27f62e345eb63a1f895b12263d77295342523a08
chihuo2104 <chihuoqwefengmi13@qq.com>
2021-10-24 11:43:27 +0800

    Future:Add some small eggs on some special events.

--- src/components/IndexCard.vue
+++ src/components/IndexCard.vue
@@ -2,7 +2,9 @@
   <div id="app"  class="allwidth">
     <Card class="scroll">
       <h1>个人网站</h1>
-      <div v-if="((new Date().getMonth() + 1) == 10) && ((new Date().getDate()) == 24 )">Happy 1024 Coding !</div>
+      <div v-if="((new Date().getMonth() + 1) == 10) && ((new Date().getDate()) == 24 )">Happy 1024 Coding!🎉</div>
+      <div v-if="((new Date().getMonth() + 1) == 10) && ((new Date().getDate()) == 31 )">Happy Halloween!🎃🎉</div>
+      <div v-if="((new Date().getMonth() + 1) == 12) && ((new Date().getDate()) == 25 )">Merry Christmas!🎅🎉</div>
       <h2>提示！</h2>
       <div>本站目前还正在开发中，为您展示的是正在开发的版本，本站暂定版本号：Moe Edition。（版本更新记录详见
         <a href="//history.moekonnyaku.com/newer-log.html" target="_blank">history.moekonnyaku.com/newer-log.html</a>）建议您去我的
