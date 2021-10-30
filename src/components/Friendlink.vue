2c3ed80f6926cf397db9808a14e6667130685d95
chihuo2104 <chihuo2104@moekonnyaku.com>
2021-10-30 16:59:25 +0800

    Chores:Add some friend links

--- src/components/Friendlink.vue
+++ src/components/Friendlink.vue
@@ -2,6 +2,8 @@
   <div id="app"  class="allwidth">
     <Card class="scroll">
       <h1>友情链接</h1>
+      <a href="//raycoder.me" target="_blank">Ray's Blog</a><br/>
+      <a href="//blog.chihuo2104.dev" target="_blank">chihuo2104の部落格</a>
       <h2>已加入萌备，备案号：萌ICP备-20212104号</h2>
       <h2>占位符啦啦啦啦啦</h2>
     </Card>
