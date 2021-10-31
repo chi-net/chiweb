d80e72424e10f6487656049efef50839666adf51
chihuo2104 <chihuoqwefengmi13@qq.com>
2021-10-31 13:04:17 +0800

    Fixed:package date

--- src/components/Copyright.vue
+++ src/components/Copyright.vue
@@ -2,7 +2,7 @@
   <div id="copyright" :class="{'hidden':this.$store.state.isCopyrightHidden}">
     Powered by <a href="//im.chihuo2104.dev" target="_blank">chihuo2104</a> &amp; <a href="//raycoder.me" target="blank">Ray</a>. All rights reserved &copy;2018-{{new Date().getFullYear()}}.
     <br>
-    <a href="//github.com/chihuo2104/chiweb/" target="_blank">Chiweb</a> Application Version:V3.0(Moe)[Preview Edition].Packaged Date:2021-10-3.
+    <a href="//github.com/chihuo2104/chiweb/" target="_blank">Chiweb</a> Application Version:V3.0(Moe)[Preview Edition].Packaged Date:2021-10-30.
     <br>
     Engined by <a href="//cn.vuejs.org" target="blank">Vue</a>&amp;<a href="//nodejs.org" target="blank">Node.js</a>&amp;<a href="//webpack.js.org" target="blank">Webpack</a>&amp;<a target="_blank" href="//jenkins.io">Jenkins</a>.<a href="//icp.gov.moe/?keyword=20212104" target="_blank">{{$t('moenum')}}</a>
     <br>{{$t('showalive')}}{{aliveshow}}<!--<span id="checkday">0</span>天<span id="checkhour">0</span>小时<span id="checkmin">0</span>分钟<span id="checksec">0</span>秒--><br>
