ed8288406d298c74aea601373bd29075cdaa4057
chihuo2104 <chihuo2104@moekonnyaku.com>
2021-11-27 19:16:34 +0800

    feat:Chose lang

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -15,9 +15,9 @@
       <a @click="changeCH()" id="showcopyright">{{$t('showcpr')}}</a>
     </Card>
     <Card id="translations">
-      <div id="translations-img"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5h12M9 3v2m1.048 9.5A18.022 18.022 0 016.412 9m6.088 9h7M11 21l5-10 5 10M12.751 5C11.783 10.77 8.07 15.61 3 18.129"></path></svg></div>
-      <a id="zh" @click="locate('cn')">简体中文</a><br>
-      <a id="en" @click="locate('en')">English</a>
+      <span id="translations-img"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5h12M9 3v2m1.048 9.5A18.022 18.022 0 016.412 9m6.088 9h7M11 21l5-10 5 10M12.751 5C11.783 10.77 8.07 15.61 3 18.129"></path></svg></span>
+      <a id="zh" @click="locate('cn')" :class="{ chose: (this.$i18n.locale == 'cn')?true:false }">简体中文</a>&nbsp;
+      <a id="en" @click="locate('en')" :class="{ chose: (this.$i18n.locale == 'en')?true:false }">English</a>
     </Card>
   </div>
 </template>
@@ -76,19 +76,19 @@ export default {
 .btn-group a {
   text-align:center;
   width:33%;
+  vertical-align: middle;
 }
 #chiimg {
   border-radius: 256px;
   width: 40%;
-}
-#translations{
-  display:block;
-  /* cursor:pointer; */
 }
 #translations a{
   cursor:pointer;
 }
 #translations-img svg{
   height: 32px;
+}
+.chose{
+  color: red !important;
 }
 </style>
