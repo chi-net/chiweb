fef70864131a4bed5628e877f8ed15b9b480269c
chihuo2104 <chihuo2104@moekonnyaku.com>
2021-12-31 20:00:16 +0800

    chore: release 3.3

--- src/components/Copyright.vue
+++ src/components/Copyright.vue
@@ -3,7 +3,7 @@
     Powered by <a href="//im.chihuo2104.dev" target="_blank">chihuo2104</a>.
     All rights reserved &copy;2018-{{new Date().getFullYear()}}.
     <br>
-    <a href="//github.com/chihuo2104/chiweb/" target="_blank">Chiweb</a> Application Version:V3.3-Preview(Moe).
+    <a href="//github.com/chihuo2104/chiweb/" target="_blank">Chiweb</a> Application Version:V3.3(Moe).
     Package Date:2021-12-31.
     <br>
     Engined by <a href="//cn.vuejs.org" target="blank">Vue</a>&amp;
