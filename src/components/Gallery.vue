ccaeb2daa829b3cc8ed4471277ae538601362bc1
chihuo2104 <chihuo2104@moekonnyaku.com>
2022-01-01 11:14:15 +0800

    feat: gallery img

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="gallery">
-    <img id="bg" :src="imgpath" :title="imgdesc">
+    <img id="gallery-img" :src="imgpath" :title="imgdesc">
     <!-- <img id="sm" src="../assets/gallerys/arknights/sm-1.png"> -->
   </div>
 </template>
@@ -19,35 +19,16 @@ export default {
 }
 </script>
 <style lang="less" scoped>
-#bg,#sm{
-  padding:0px;
+#gallery-img{
+  padding: 0px;
   margin: 0px;
-  width:100%;
-  height:100%;
-  z-index:-1;
-}
-#gallery{
-  z-index:-1;
-  width:100%;
-  height:100%;
-  padding:0px;
-  margin:0px;
-}
-@media screen and (min-width:768px){
-  #sm{
-    display:none;
-  }
-  #bg{
-    position: fixed;
-    top: 0px;
-    bottom: 0px;
-    left: 0px;
-    right: 0px;
-  }
-}
-@media screen and (max-width:768px){
-  #bg{
-    display:none;
-  }
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  position: fixed;
+  bottom: 0px;
+  top: 0px;
+  left: 0px;
+  right: 0px;
 }
 </style>
